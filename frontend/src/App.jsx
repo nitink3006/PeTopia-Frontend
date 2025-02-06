505782bf84c2a3d6879e482d8d9e5645b1f59eef
KARAN KUMAR GUPTA <karang152btechaiml2025@kccitm.edu.in>
2025-02-06 15:33:37 +0530

    admin-pannel
    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar/Navbar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
