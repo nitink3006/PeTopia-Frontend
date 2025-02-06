3a1f3a9bc3a1dae89f0703352b338c7b60639451
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2025-02-06 15:14:26 +0530

    Revert "Admin pannel (#5)" (#6)
    This reverts commit fcf04c58781dfccef1ebe7c59989e312a66eb02f.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,74 +1,18 @@
-import React from "react";
-import {Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavBar/Navbar";
-import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
-import Services from "./components/Services/Services";
-import Contact from "./components/Contact/Contact";
-import Pets from "./components/Pets/Pets";
-import AdoptForm from "./components/AdoptForm/AdoptForm";
-import AdminLogin from "./components/AdminPanel/AdminLogin";
-import "./App.css";
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
 
-const Layout = ({ children }) => (
-  <>
-    <Navbar title="PawFinds" />
-    {children}
-    <Footer title="PawFinds" />
-  </>
-);
+function App() {
+ 
 
-const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            <Layout>
-              <Home description="Ensure you are fully prepared to provide proper care and attention to your pet before welcoming them into your home." />
-            </Layout>
-          } 
-        />
-        <Route 
-          path="/services" 
-          element={
-            <Layout>
-              <Services />
-            </Layout>
-          } 
-        />
-        <Route 
-          path="/contact" 
-          element={
-            <Layout>
-              <Contact />
-            </Layout>
-          } 
-        />
-        <Route 
-          path="/pets" 
-          element={
-            <Layout>
-              <Pets />
-            </Layout>
-          } 
-        />
-        <Route 
-          path="/adopt-form" 
-          element={
-            <Layout>
-              <AdoptForm />
-            </Layout>
-          } 
-        />
-        <Route 
-          path="/admin" 
-          element={<AdminLogin />} 
-        />
-      </Routes>
-    </Router>
-  );
-};
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  )
+}
 
-export default App;
+export default App
